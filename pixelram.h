6e8cf3f3eb0680bf666fbdb893bd733d95b5e181
Michael Specht <micha.specht@gmail.com>
2026-08-13 17:02:30 +0200

    yay

--- pixelram.h
+++ pixelram.h
@@ -11,8 +11,8 @@ extern "C" {
 
 #define PIXELRAM_VERSION_MAJOR 0
 #define PIXELRAM_VERSION_MINOR 1
-#define PIXELRAM_VERSION_PATCH 19
-#define PIXELRAM_VERSION "0.1.19"
+#define PIXELRAM_VERSION_PATCH 20
+#define PIXELRAM_VERSION "0.1.20"
 
 /*
  * PixelRAM is a small software framebuffer API.
